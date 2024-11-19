41afbab2c23e428f34f4590511860db697109ded
Yuji Tani <valleyyuji.webengineer@gmail.com>
2024-11-19 22:58:17 +0900

    Redisを使ってDBへ初期データを保存する

--- api/server2.js
+++ api/server2.js
@@ -7,9 +7,19 @@ const app = express();
 const redis = new Redis({
   host: "localhost", // デフォルトのホスト
   port: 6379, // デフォルトのポート
-  password: process.env.REDIS_PASSWORD,
+  password: process.env.REDIS_PASSWORD, // 一旦実行時に環境変数を設定する
   enableOfflineQueue: false, // デフォルトのオフラインキュー
 });
+
+// 初期データをRedisに挿入
+const init = async () => {
+  await Promise.all([
+    redis.set("users:1", JSON.stringify({ id: 1, name: "alpha" })),
+    redis.set("users:2", JSON.stringify({ id: 2, name: "bravo" })),
+    redis.set("users:3", JSON.stringify({ id: 3, name: "charlie" })),
+    redis.set("users:4", JSON.stringify({ id: 4, name: "delta" })),
+  ]);
+};
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello World\n");
@@ -19,8 +29,11 @@ app.get("/users/:id", (req, res) => {
   res.status(200).send(`User id: ${req.params.id}`);
 });
 
-redis.once("ready", () => {
+redis.once("ready", async () => {
   try {
+    // 初期データをRedisに挿入してからサーバーを起動
+    await init();
+
     app.listen(3010, () => {
       console.log("Server is running on port 3010");
     });
